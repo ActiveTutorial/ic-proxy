569e7c77ea123bb06db6a00d50b53d903191a923
John Doe <johndoe@email.com>
2025-10-05 20:37:29 +0200

    Change referrer

--- index.mjs
+++ index.mjs
@@ -10,10 +10,13 @@ export function pair(first, second) {
   if (!first || !second) {
     return Promise.reject(new Error('Both "first" and "second" parameters are required.'));
   }
-  const url = `https://neal.fun/api/infinite-craft/pair?ref=app&first=${encodeURIComponent(first)}&second=${encodeURIComponent(second)}`;
+  const url = `https://neal.fun/api/infinite-craft/pair?first=${encodeURIComponent(first)}&second=${encodeURIComponent(second)}`;
   return req.get(url, {
     impersonate: "chrome136",
-    userAgent: "M"
+    userAgent: "M",
+    headers: {
+      Referer: "https://neal.fun/infinite-craft/"
+    }
   })
     .then(r => {
       if (r.data) {
@@ -34,10 +37,13 @@ export function check(first, second, result) {
   if (!first || !second || !result) {
     return Promise.reject(new Error('All "first", "second", and "result" parameters are required.'));
   }
-  const url = `https://neal.fun/api/infinite-craft/check?ref=app&first=${encodeURIComponent(first)}&second=${encodeURIComponent(second)}&result=${encodeURIComponent(result)}`;
+  const url = `https://neal.fun/api/infinite-craft/check?first=${encodeURIComponent(first)}&second=${encodeURIComponent(second)}&result=${encodeURIComponent(result)}`;
   return req.get(url, {
     impersonate: "chrome136",
-    userAgent: "M"
+    userAgent: "M",
+    headers: {
+      Referer: "https://neal.fun/infinite-craft/"
+    }
   })
     .then(r => {
       if (r.data) {
